b3ff5b7a411dbe373cefdd771133932568c04144
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-09-09 17:43:00 +0530

    done

---
+++ controllers/userController.js
@@ -0,0 +1,82 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const bcryptSalt = bcrypt.genSaltSync(10);
+require("dotenv").config();
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc).status(200);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+};
+
+
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        {
+          email: userDoc.email,
+          id: userDoc._id,
+        },
+        process.env.JWT_SECRETT,
+        { expiresIn: "1h" }, // Expires in 1 hour,
+        (err, token) => {
+          if (err) throw err;
+          res
+            .cookie("token", token, {
+              httpOnly: true, // Only accessible via HTTP requests, not JS
+              secure: true, // Must be true for HTTPS (set this in production)
+              sameSite: "None",
+            })
+            .json(userDoc);
+        }
+      );
+    } else {
+      res.status(422).json("pass not ok");
+    }
+  } else {
+    res.status(422).json("not found");
+  }
+};
+
+// const profile = (req, res) => {
+//   const { token } = req.cookies;
+//   if (token) {
+//     jwt.verify(token, process.env.JWT_SECRET, {}, async (err, userData) => {
+//       if (err) throw err;
+//       const { name, email, _id } = await User.findById(userData.id);
+//       res.json({ name, email, _id }).status(200);
+//     });
+//   } else {
+//     res.json(null).status(401);
+//   }
+// };
+
+const profile = async (req, res) => {
+  const userId = req.userId;
+  try {
+    const { name, email, _id } = await User.findById(userId);
+    res.json({ name, email, _id }).status(200);
+  } catch (e) {
+    res.json(null).status(401);
+  }
+};
+
+const logout = (req, res) => {
+  res.clearCookie("token").json(true).status(200);
+};
+module.exports = { register, login, profile, logout };
